5932107fa18c437d57c256fcacbb6a73668d8052
Chris Thierauf <chris@cthierauf.com>
2019-08-05 17:29:03 -0400

    Update connect4_logic.asm

--- connect4_logic.asm
+++ connect4_logic.asm
@@ -17,6 +17,8 @@ INCLUDE GraphWin.inc
 	; The last point at which 'x' and 'y' was placed.
 	m_lastx db 0
 	m_lasty db 0
+	m_last_tokenx db 0
+
 
 	; Last values for things so we don't accidently write them over 
 	; when running through a method
@@ -43,6 +45,40 @@ INCLUDE GraphWin.inc
 	matval_coordy db 0
 	g_coordx db 0
 	g_coordy db 0
+
+	cqs_slotslice db 4,3,3,3,4,1,3,1,1
+	cqs_tokenslice db 1,3,3,3,3,3,3,3,4 
+	cqs_slotslicex db 3
+	cqs_slotslicey db 3
+	cqs_rows db 0
+	cqs_columns db 0
+	cqs_slice dd 0
+
+	ca_slotraw db 36 DUP(0)
+	ca_tokenraw db 36 DUP(0)
+	ca_barleftRaw db 2,3,4,4
+	ca_barRightRaw db 4 Dup(0)
+
+	g_token db 72 DUP(0)
+	g_tokenslot db 72 DUP(0)
+	g_slotrows db 6
+	g_slotcolumns db 12
+
+	g_barRight db 8 Dup(0)
+	g_barLeft db 8 Dup(0)
+
+	g_barRows db 1
+	g_barcolumns db 8
+
+	ps_int db 0
+	ps_rows db 0
+	ps_columns db 0
+	ps_x db 0
+	ps_y db 0
+
+	pb_loop db 0
+
+	pg_startingrow db 0
 	
 	; The number of turns we've had. This way we can detect a stalemate, and
 	; we can tell whose turn it is.
@@ -55,6 +91,7 @@ INCLUDE GraphWin.inc
 	printmatbar db" ----------------", 13, 10, 0
 	youwonstring db "Player won!", 13, 10, 0
 	invalidclick db "Invalid click location!", 13, 10, 0
+	blanks db "                       ", 13, 10, 0
 
 	hStdIn    dd 0
     nRead     dd 0
@@ -78,9 +115,12 @@ INCLUDE GraphWin.inc
 ;=================== CODE =========================
 .code
 main PROC
+	; Set up the background stuff
+	call initsprites
+	call printbarright
+	call printbarleft
+	call printgrid
 
-	; Mouse interaction was informed by https://stackoverflow.com/questions/33973305,
-	; modified to give an x/y coordinate and integrated into the whole program.
     invoke GetStdHandle,STD_INPUT_HANDLE
     mov   hStdIn,eax
 
@@ -96,11 +136,12 @@ main PROC
 
         movzx  eax,InputRecord.EventType
         cmp eax, MOUSE_EVENT
+
         jne endloop
         mov eax, InputRecord.MouseEvent.dwMousePosition
         test InputRecord.MouseEvent.dwButtonState, 1
 
-        jz endloop
+        jz endloop_justmove
 
 		call calculatecolumn
 		cmp al, -1
@@ -110,14 +151,47 @@ main PROC
 		cmp al, -1
 		je m_flagval
 
-		call printmat
+		mov dl, g_coordy
+		mov dh, g_coordx
+		mov al, dh
+		mov ah, dl
+		call convertrowcol
+		call printtoken
+		call printtokenslot
+		call writehex
+
 		inc g_turn
 		jmp endloop
 
 		m_flagval:
 			mov edx, offset invalidclick
 			call writestring
+			mov edx,0
+			Call gotoxy
+			mov eax, 500
+			call delay
+			mov edx, offset blanks
+			call writestring
+			mov edx, 0
+			call gotoxy
+			jmp endloop
 
+		endloop_justmove:
+			cmp al, 12*7+8-6
+			jge endloop
+			cmp al, 12
+			jle endloop
+
+			mov dl, m_last_tokenx
+			mov dh, 0
+			call cleartoken
+
+			mov dl, al
+			sub dl, 5
+			mov m_last_tokenx, dl
+			mov dh, 0
+			call printtoken
+			mov edx, 0
         endloop:
     jmp m_mainloop
 
@@ -129,6 +203,49 @@ main PROC
     call ReadChar
     invoke ExitProcess, 0
 main ENDP
+
+ConvertRowCol PROC
+    push ebx
+    push ecx
+    push edx
+    mov edx, 0
+    push eax
+    
+    ;max
+    push eax
+    mov eax, 0
+    mov al, g_slotrows
+    mov bl, 6
+    mov ecx, eax    
+    pop eax
+    
+    mov ebx, 0
+    and eax, 0000ff00h
+    mov al, ah
+    mov ah, 0
+    mov bl, g_slotrows
+    mul bl
+    ; sub the max from the current value
+    sub cl, al
+    mov al, cl
+    mov dh, al
+
+    pop eax
+    push eax
+    and eax, 000000ffh
+    mov bl, g_slotcolumns
+    mul bl
+    add al, g_barcolumns
+    mov dl, al
+    pop eax
+    mov ax, dx
+
+    pop edx
+    pop ecx
+    pop ebx
+
+    ret
+ConvertRowCol ENDP
 
 ;=== For the sake of easy debug, print the current player
 printcurrentplayer proc
@@ -703,7 +820,7 @@ placecoin proc
 		jmp placecoin_findcoinloop
 
 	placecoin_maxedout:
-		mov ah, -1
+		mov al, -1
 		mov ebx, g_last_ebx
 		mov ecx, g_last_ecx
 		ret
@@ -723,8 +840,507 @@ placecoin proc
 	ret
 placecoin endp
 
-;=== Get where the user wants to place a coin via text input, to be substituted by mouse input later
-getcol proc
-getcol endp
+
+InitSprites PROC
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push esi
+	push edi
+	mov ecx, lengthof ca_barleftraw
+	mov esi, offset ca_barLeftraw
+	mov edi, offset ca_barRightraw
+	add edi, ecx
+	dec edi
+is_fillBarRight:
+	mov al, [esi]
+	mov [edi], al
+	inc esi
+	dec edi
+	Loop is_fillBarRight
+
+	mov esi, offset cqs_tokenslice
+	mov edi, offset ca_tokenRaw
+	mov bl, cqs_slotslicey
+	mov bh, cqs_slotslicex
+	Call convertquartersprite
+	mov esi, Offset ca_tokenRaw
+	mov edi, Offset g_token
+	mov ecx, lengthof ca_tokenRaw
+	Call changearray
+
+	mov esi, offset cqs_slotslice
+	mov edi, offset ca_slotraw
+	Call convertquartersprite
+	mov esi, Offset ca_slotraw
+	mov edi, Offset g_tokenslot
+	mov ecx, lengthof ca_slotraw
+	Call changearray
+
+
+	mov esi, Offset ca_barRightraw
+	mov edi, Offset g_barRight
+	mov ecx, lengthof ca_barRightraw
+	Call changearray
+
+	mov esi, Offset ca_barleftraw
+	mov edi, Offset g_barleft
+	mov ecx, lengthof ca_barleftraw
+	Call changearray
+	
+	pop edi
+	pop esi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+InitSprites ENDP
+
+PrintTokenSlot PROC
+	;dh has current row, dl has current column
+	push esi
+	push ebx
+	push ecx
+	push eax
+	mov eax, yellow
+	call setTextColor
+	mov esi, offset g_tokenslot
+	mov ebx, 0
+	mov bl, g_slotrows
+	mov ecx, 0
+	mov cl, g_slotcolumns
+	Call PrintSprite
+	mov eax, white
+	call setTextColor
+	pop eax
+	pop ecx
+	pop ebx
+	pop esi
+	ret
+PrintTokenSlot endp
+
+
+printBarRight PROC
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push esi
+	push edi
+
+	mov eax, yellow
+	call setTextColor
+
+	mov eax, 0
+	mov ebx, 0
+	mov ecx, 0
+	mov edx, 0
+	mov esi, offset g_barright
+	
+	mov al, g_slotcolumns
+	mov bl, 7
+	mul bl
+	add al, g_barcolumns
+	mov dl, al
+
+	mov dh, g_slotrows
+	mov eax, 7
+	mov cl, g_barcolumns
+	mov bl, g_slotrows
+	mul bl
+	mov bl, g_barrows
+	mov pb_loop, al
+	xchg pb_loop, cl
+pbr_sideloop:
+	xchg pb_loop, cl
+	call PrintSprite
+	inc dh
+	xchg pb_loop, cl
+	Loop pbr_sideloop
+	
+	mov eax, white
+	call setTextColor
+
+	pop edi
+	pop esi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+printbarRight endp
+
+printbarLeft PROC
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push esi
+	push edi
+
+	mov eax, yellow
+	call setTextColor
+
+	mov eax, 0
+	mov ebx, 0
+	mov ecx, 0
+	mov edx, 0
+	mov esi, offset g_barleft
+	
+	mov dh, g_slotrows
+	mov eax, 7
+	mov cl, g_barcolumns
+	mov bl, g_slotrows
+	mul bl
+	mov bl, g_barrows
+	mov pb_loop, al
+	xchg pb_loop, cl
+pbl_sideloop:
+	xchg pb_loop, cl
+	call PrintSprite
+	inc dh
+	xchg pb_loop, cl
+	Loop pbl_sideloop
+	
+	mov eax, white
+	call setTextColor
+
+	pop edi
+	pop esi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+printbarLeft ENDP
+
+
+PrintGrid PROC
+	push ebx
+	push ecx
+	push edx
+	mov edx, 0
+	add dl, g_barcolumns
+	add dh, g_slotrows
+	mov ecx, 7
+	mov ebx, 7
+	mov pg_startingrow, dl
+pg_startall:
+	dec bl
+	cmp bl, 0
+	jle pg_done
+pg_loop:
+	Call PrintTokenSlot
+	add dl, g_slotcolumns
+	Loop pg_loop
+
+	add dh, g_slotrows
+	mov dl, pg_startingrow
+	mov ecx, 7
+	jmp pg_startall 
+pg_done:
+	pop edx
+	pop ecx
+	pop ebx
+	ret
+PrintGrid endp
+
+
+
+ClearTokenSlot PROC
+	;dh has current row, dl has current column
+	push ebx
+	push ecx
+	mov ebx, 0
+	mov bl, g_slotrows
+	mov ecx, 0
+	mov cl, g_slotcolumns
+	Call clearSprite
+	pop ecx
+	pop ebx
+	ret
+ClearTokenSlot endp
+
+
+
+PrintToken PROC
+	;dh has current row, dl has current column
+	push esi
+	push ebx
+	push ecx
+	push eax
+
+	call getcurrentplayer
+	cmp al, 1
+	je printtoken_playerone
+	cmp al, 2
+	je printtoken_playertwo
+	
+	printtoken_playerone:
+		mov eax, red
+		jmp printtoken_colorselected
+	printtoken_playertwo:
+		mov eax, blue
+		jmp printtoken_colorselected
+
+	printtoken_colorselected:
+	call setTextColor
+	;dh has current row, dl has current column
+	mov esi, offset g_token
+	mov ebx, 0
+	mov bl, g_slotrows
+	mov ecx, 0
+	mov cl, g_slotcolumns
+	Call PrintSprite
+	mov eax, white
+	call setTextColor
+	pop eax
+	pop ecx
+	pop ebx
+	pop esi
+	ret
+PrintToken endp
+
+
+
+ClearToken PROC
+	call ClearTokenslot
+	ret
+ClearToken endp
+
+
+
+convertquartersprite PROC
+	; esi has the quarter
+	; edi has the receptacle
+	; bl has the rows of slice, bh has the columns of slice
+	
+	; dl will be row counter, dh will be column counter
+	
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push esi
+	push edi
+
+	mov cqs_slice, esi
+	mov ecx, 0
+	mov cl, bl
+	mov dx, 0
+
+cqs_loopUpLeft:
+	mov al, [esi] 
+	mov [edi], al
+	inc edi
+	inc esi
+	inc dl
+	cmp dl, bh
+	jl cqs_loopupleft
+	dec esi
+	mov dl, 0
+cqs_loopUpRight:
+	mov al, [esi] 
+	mov [edi], al
+	inc edi
+	dec esi
+	inc dl
+	cmp dl, bh
+	jl cqs_loopupright
+	inc esi
+	push eax
+	mov eax, 0
+	mov al, bh
+	add esi, eax
+	pop eax
+	mov dl, 0
+	Loop cqs_loopUpLeft
+	
+	push eax
+	mov eax, 0
+	mov al, bh
+	sub esi, eax
+	pop eax
+	mov cl, bl
+
+cqs_loopDownLeft:
+	mov al, [esi] 
+	mov [edi], al
+	inc edi
+	inc esi
+	inc dl
+	cmp dl, bh
+	jl cqs_loopDownleft
+	dec esi
+	mov dl, 0
+cqs_loopDownRight:
+mov al, [esi] 
+	mov [edi], al
+	inc edi
+	dec esi
+	inc dl
+	cmp dl, bh
+	jl cqs_loopDownright
+	inc esi
+	push eax
+	mov eax, 0
+	mov al, bh
+	sub esi, eax
+	pop eax
+	mov dl, 0
+	Loop cqs_loopDownLeft
+	pop edi
+	pop esi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+convertquartersprite endp
+
+
+printSprite PROC
+	;bl has rows
+	;cl has columns
+	;dh has current row, dl has current column
+	;esi has total image
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push esi
+
+	mov ps_x, dl
+	mov ps_y, dh
+	mov ps_int, cl
+	mov ps_rows, dl
+	mov ps_columns, dh
+ps_startall:
+	Call gotoXY
+	dec bl
+	cmp bl, 0
+	jl ps_endprint
+
+ps_printloop:
+	mov al, [esi]
+	inc dl
+	cmp al, 32
+	je ps_dontprint
+	Call WriteChar
+ps_dontprint:
+	Call gotoxy
+	inc esi
+		loop ps_printloop
+	mov dl, ps_x
+	inc dh
+	call gotoxy
+	mov cl, ps_int
+	jmp ps_startall
+ps_endprint:
+	mov edx,0
+	Call gotoXY
+	pop esi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	
+	ret
+printSprite endp
+
+
+changeArray PROC
+	;esi has source
+	;edi has destination
+	;ecx has size of array
+	push eax
+	push ebx
+	push ecx
+	push edx
+	push esi
+	push edi
+ca_start:
+	mov bl, [esi]
+	mov dl, 1
+	cmp [esi],dl
+	jne ca_d1
+	mov bl, 32
+	jmp ca_doneloop
+ca_d1:
+	mov dl, 2
+	cmp [esi], dl
+	jne ca_d2
+	mov bl, 176
+	jmp ca_doneloop
+ca_d2:
+	mov dl, 3
+	cmp [esi], dl
+	jne ca_d3
+	mov bl, 177
+	jmp ca_doneloop
+ca_d3:
+	mov dl, 4
+	cmp [esi], dl
+	jne ca_d4
+	mov bl, 178
+	jmp ca_doneloop
+ca_d4:
+	mov dl, 5
+	cmp [esi], dl
+	jne ca_doneloop
+	mov bl, 219
+ca_doneloop:
+	mov [edi], bl
+	inc edi
+	mov [edi], bl
+	inc edi
+	inc esi
+	loop ca_start
+
+	pop edi
+	pop esi
+	pop edx
+	pop ecx
+	pop ebx
+	pop eax
+	ret
+changeArray endp
+
+ClearSprite PROC
+	;dh has current row, dl has current column
+	;bh has num row, bl has numcolumn
+	push edx
+	push ebx
+	push ecx
+	push eax
+	
+	mov bh, cl
+	mov ecx, 0
+	mov eax, 32
+	Call gotoxy
+cs_startall:
+	mov cl, bh
+	dec bl
+ 	cmp bl, 0
+	jl cs_doneall
+
+cs_printloop:
+	Call WriteChar
+	Loop cs_printLoop
+	
+	inc dh
+	Call gotoxy
+	jmp cs_startall
+cs_doneall:
+	mov edx,0
+	Call gotoxy
+	pop eax
+	pop ecx
+	pop ebx
+	pop edx
+	ret
+ClearSprite ENDP
 
 end main
